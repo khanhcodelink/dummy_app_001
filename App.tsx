976a82b142beb01b66c25b5af85ef838668759f6
Khanh Nguyen <khanh.nguyen@codelink.io>
2021-12-11 21:11:45 +0700

    change to typescript

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import DataInputScreen from './src/screens/DataInputScreen/DataInputScreen';
+import DefinitionScreen from './src/screens/DefinitionScreen/DefinitionScreen';
+
+const Stack = createNativeStackNavigator();
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={DataInputScreen} />
+        <Stack.Screen
+          name="Definition"
+          component={DefinitionScreen}
+          initialParams={{definition: 'Please input data'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
